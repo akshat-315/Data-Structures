686ab4d3c7d98a442d3fb4181902794ceee004db
Akshat <130206602+akshat-315@users.noreply.github.com>
2023-07-08 02:08:40 +0530

    Add files via upload

---
+++ Arrays/best_time_to_buy_and_sell_stocks.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class best_time_to_buy_and_sell_stocks {
+    public static int maxProfit(int[] prices) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (buyPrice < prices[i]) {
+                int profit = prices[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String args[]) {
+        int prices[] = { 7, 1, 5, 3, 6, 4 };
+        int answer = maxProfit(prices);
+
+        System.out.print("Maximum profit is : " + answer);
+    }
+}
