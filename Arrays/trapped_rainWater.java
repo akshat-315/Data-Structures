84c6aa866a98f352c36be6b6df82c4e8d8bae0d6
Akshat <130206602+akshat-315@users.noreply.github.com>
2023-07-08 00:02:56 +0530

    Update trapped_rainWater.java

--- Arrays/trapped_rainWater.java
+++ Arrays/trapped_rainWater.java
@@ -4,15 +4,15 @@ public class trapped_rainWater {
     public static int trapped(int[] height) {
         int n = height.length;
 
-        int leftMax[] = new int[n];
-        leftMax[0] = height[0];
+        int leftMax[] = new int[n];               //making a helper array for left Max Boundary
+        leftMax[0] = height[0];                    
         for (int i = 1; i < n; i++) {
             leftMax[i] = Math.max(height[i], leftMax[i - 1]);
         }
 
-        int rightMax[] = new int[n];
+        int rightMax[] = new int[n];              //making a helper array for right Max Boundary
         rightMax[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
+        for (int i = n - 2; i >= 0; i--) {         
             rightMax[i] = Math.max(height[i], rightMax[i + 1]);
         }
 
